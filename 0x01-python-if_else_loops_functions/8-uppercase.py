35d622dccfe6607a6e7b33c98dddac0522d17923
Ngumi22 <mwaurapngumi@gmail.com>
2024-01-03 00:07:13 +0300

    Correct errors

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uppercase(s):
+    output = ""
     for char in s:
         uppercase_char = chr(ord(char) - 32) if 'a' <= char <= 'z' else char
-        print(uppercase_char, end='')
-    print()
+        output += uppercase_char
+    print("{}".format(output))
