5e316288a6f8d2006b6a3959a2831ba5d3b02e10
Ngumi22 <mwaurapngumi@gmail.com>
2024-01-05 09:27:57 +0300

    0x02. Python - import & modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,21 +1,9 @@
 #!/usr/bin/python3
-import dis
-
-
-def print_hidden_names(module_file):
-    with open(module_file, 'rb') as file:
-        bytecode = file.read()
-
-    instructions = dis.get_instructions(bytecode)
-    names = set()
-
-    for instr in instructions:
-        if instr.opname == 'LOAD_NAME' and not instr.arg.startswith('__'):
-            names.add(instr.arg)
-
-    for name in sorted(names):
-        print(name)
-
 
 if __name__ == "__main__":
-    print_hidden_names("hidden_4.pyc")
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
