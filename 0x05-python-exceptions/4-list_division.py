e4293f7618806623173d8eebdc34f1843f18f333
Ngumi22 <mwaurapngumi@gmail.com>
2024-01-22 23:19:25 +0300

    0x05. Python - Exceptions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,7 +6,8 @@ def list_division(my_list_1, my_list_2, list_length):
         result = 0
 
         try:
-            if type(my_list_1[i]) not in (int, float) or type(my_list_2[i]) not in (int, float):
+            if type(my_list_1[i]) not in (int, float)
+            or type(my_list_2[i]) not in (int, float):
                 raise ValueError("wrong type")
 
             result = my_list_1[i] / my_list_2[i]
