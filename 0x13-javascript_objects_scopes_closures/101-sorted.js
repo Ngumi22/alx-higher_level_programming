0a9019644323f7050cc64b78f7e3eaea5d006efe
Ngumi22 <mwaurapngumi@gmail.com>
2024-04-10 19:13:18 +0300

    javascript_objects_scopes_closures

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const { dict } = require('./101-data');
+
+const sortedDict = {};
+
+for (const [userId, occurrences] of Object.entries(dict)) {
+  if (!sortedDict[occurrences]) {
+    sortedDict[occurrences] = [];
+  }
+  sortedDict[occurrences].push(userId);
+}
+console.log(sortedDict);
