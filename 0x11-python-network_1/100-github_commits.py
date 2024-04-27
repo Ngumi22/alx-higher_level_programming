c1a6b402bce05c77ee429f137423692a0c0ca690
Ngumi22 <mwaurapngumi@gmail.com>
2024-04-27 19:53:23 +0300

    Corrections

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,18 +4,20 @@ of the repository “rails” by the user “rails”"""
 import requests
 import sys
 
-repo_name = sys.argv[1]
-owner_name = sys.argv[2]
 
-url = f'https://api.github.com/repos/{owner_name}/{repo_name}/commits'
+if __name__ == "__main__":
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
 
-response = requests.get(url)
+    url = f'https://api.github.com/repos/{owner_name}/{repo_name}/commits'
 
-if response.status_code == 200:
-    commits = response.json()
-    for commit in commits[:10]:
-        sha = commit['sha']
-        author_name = commit['commit']['author']['name']
-        print(f"{sha}: {author_name}")
-else:
-    print("Error:", response.status_code)
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        commits = response.json()
+        for commit in commits[:10]:
+            sha = commit['sha']
+            author_name = commit['commit']['author']['name']
+            print(f"{sha}: {author_name}")
+        else:
+            print("Error:", response.status_code)
